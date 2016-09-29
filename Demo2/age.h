fbb6e8271887c509de447977472ade5355263a0a
Maximiliano Tomasello <maximiliano.tomasello.utn@gmail.com>
2016-09-29 22:37:59 +0200

    Fix ranges

--- Demo2/age.h
+++ Demo2/age.h
@@ -7,14 +7,18 @@ class Age{
 public:
     enum Range{
         TEENAGE,
+        YOUNG_ADULT,
         ADULT,
         OLD
     };
     static Range getRangeFromAge(int age){
-        if(age<=15){
+        if(age<=24){
             return TEENAGE;
         }
-        if(age< 80){
+        if(age<=43){
+            return YOUNG_ADULT;
+        }
+        if(age<=70){
             return ADULT;
         }
         return OLD;
@@ -26,6 +30,7 @@ public:
         switch (anEnum)
         {
             case TEENAGE: result = "Adolescente"; break;
+            case YOUNG_ADULT: result = "Adulto Joven"; break;
             case ADULT: result = "Adulto"; break;
             case OLD: result = "Mayor"; break;
             default: result = "Unknown"; break;
